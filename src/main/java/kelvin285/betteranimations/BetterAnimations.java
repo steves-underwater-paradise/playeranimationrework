157a5633529596962ede7ed5c64945e9050f1ae5
Steveplays28 <djspaargaren@outlook.com>
2023-10-15 19:54:39 +0200

    feat: Set up logger

--- src/main/java/kelvin285/betteranimations/BetterAnimations.java
+++ src/main/java/kelvin285/betteranimations/BetterAnimations.java
@@ -1,13 +1,16 @@
 package kelvin285.betteranimations;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BetterAnimations implements ClientModInitializer {
-
-    public static String MOD_ID = "betteranimations";
+	public static final String MOD_ID = "betteranimations";
+	public static final String MOD_NAME = "Player Animation Rework";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
-
+	    LOGGER.info("Loading {}.", MOD_NAME);
     }
 }
