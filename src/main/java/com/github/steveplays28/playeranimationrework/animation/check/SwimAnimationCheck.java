92e44e22581c5df92deb817074aa691c29e607dc
Steveplays28 <djspaargaren@outlook.com>
2023-10-17 20:44:11 +0200

    fix: Fix and improve swimming animation

--- src/main/java/com/github/steveplays28/playeranimationrework/animation/check/SwimAnimationCheck.java
+++ src/main/java/com/github/steveplays28/playeranimationrework/animation/check/SwimAnimationCheck.java
@@ -2,29 +2,31 @@ package com.github.steveplays28.playeranimationrework.animation.check;
 
 import com.github.steveplays28.playeranimationrework.animation.AnimationData;
 import com.github.steveplays28.playeranimationrework.animation.AnimationPriority;
-import com.github.steveplays28.playeranimationrework.PlayerAnimationRework;
-import dev.kosmx.playerAnim.core.data.KeyframeAnimation;
-import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.util.Identifier;
 
 import static com.github.steveplays28.playeranimationrework.util.AnimationUtil.getAnimation;
-import static com.github.steveplays28.playeranimationrework.util.AnimationUtil.getAnimationIdentifier;
 
 public class SwimAnimationCheck implements AnimationCheck {
+	private static final String SWIM_ANIMATION_NAME = "swimming";
 	private static final String SWIM_IDLE_ANIMATION_NAME = "swim_idle";
 
 	private boolean shouldPlay = false;
+	private String selectedAnimationName;
 
 	@Override
 	public void tick(AbstractClientPlayerEntity player) {
-		this.shouldPlay = player.isInsideWaterOrBubbleColumn() && !player.isInSwimmingPose();
+		if (player.isInSwimmingPose()) {
+			shouldPlay = true;
+			selectedAnimationName = SWIM_ANIMATION_NAME;
+		} else if (player.isInsideWaterOrBubbleColumn()) {
+			shouldPlay = true;
+			selectedAnimationName = SWIM_IDLE_ANIMATION_NAME;
+		}
 	}
 
 	@Override
 	public AnimationData getAnimationData() {
-		KeyframeAnimation animation = getAnimation(SWIM_IDLE_ANIMATION_NAME);
-		return new AnimationData(animation, 1.0f, 5);
+		return new AnimationData(getAnimation(selectedAnimationName), 1.0f, 5);
 	}
 
 	@Override
