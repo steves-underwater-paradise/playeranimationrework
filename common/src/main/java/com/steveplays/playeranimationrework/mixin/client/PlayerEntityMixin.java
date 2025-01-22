7bdb375ac21efadf0308b1daadbcbc38fc0df207
Steveplays28 <djspaargaren@outlook.com>
2025-01-22 18:16:40 +0100

    refactor: :recycle: Improve player state machine code quality

--- common/src/main/java/com/steveplays/playeranimationrework/mixin/client/PlayerEntityMixin.java
+++ common/src/main/java/com/steveplays/playeranimationrework/mixin/client/PlayerEntityMixin.java
@@ -1,10 +1,12 @@
 package com.steveplays.playeranimationrework.mixin.client;
 
+import static com.steveplays.playeranimationrework.PlayerAnimationRework.LOGGER;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.mojang.authlib.GameProfile;
 import com.steveplays.playeranimationrework.client.event.PARPlayerEvents;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -13,6 +15,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -31,118 +34,133 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 		super(type, world);
 	}
 
-	@Inject(method = "jump", at = @At(value = "TAIL"))
-	private void playeranimationrework$invokeJumpEvent(CallbackInfo ci) {
+	@Inject(method = "<init>", at = @At(value = "TAIL"))
+	private void playeranimationrework$registerEventHandlers(World world, BlockPos pos, float yaw, GameProfile gameProfile, CallbackInfo ci) {
 		if (!(((PlayerEntity) (Object) this) instanceof AbstractClientPlayerEntity)) {
 			return;
 		}
 
-		PARPlayerEvents.JUMP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
+		// Register movement event handlers
+		PARPlayerEvents.IDLE_START.register(clientPlayer -> playeranimationrework$isIdle = true);
+		PARPlayerEvents.IDLE_STOP.register(clientPlayer -> playeranimationrework$isIdle = false);
+		PARPlayerEvents.WALK_START.register(clientPlayer -> playeranimationrework$isWalking = true);
+		PARPlayerEvents.WALK_STOP.register(clientPlayer -> playeranimationrework$isWalking = false);
+		PARPlayerEvents.RUN_START.register(clientPlayer -> playeranimationrework$isRunning = true);
+		PARPlayerEvents.RUN_STOP.register(clientPlayer -> playeranimationrework$isRunning = false);
+		// Register fence movement event handlers
+		PARPlayerEvents.FENCE_IDLE_START.register(clientPlayer -> playeranimationrework$isFenceIdle = true);
+		PARPlayerEvents.FENCE_IDLE_STOP.register(clientPlayer -> playeranimationrework$isFenceIdle = false);
+		PARPlayerEvents.FENCE_WALK_START.register(clientPlayer -> playeranimationrework$isFenceWalking = true);
+		PARPlayerEvents.FENCE_WALK_STOP.register(clientPlayer -> playeranimationrework$isFenceWalking = false);
+		PARPlayerEvents.FENCE_RUN_START.register(clientPlayer -> playeranimationrework$isFenceRunning = true);
+		PARPlayerEvents.FENCE_RUN_STOP.register(clientPlayer -> playeranimationrework$isFenceRunning = false);
+	}
+
+	@Inject(method = "jump", at = @At(value = "TAIL"))
+	private void playeranimationrework$invokeJumpEvent(CallbackInfo ci) {
+		if (!(((PlayerEntity) (Object) this) instanceof AbstractClientPlayerEntity clientPlayer)) {
+			return;
+		}
+
+		PARPlayerEvents.JUMP.invoker().onExecute(clientPlayer);
 	}
 
 	@Inject(method = "tick", at = @At(value = "TAIL"))
 	private void playeranimationrework$invokePlayerStopMovementEvents(CallbackInfo ci) {
-		if (!(((PlayerEntity) (Object) this) instanceof AbstractClientPlayerEntity)) {
+		if (!(((PlayerEntity) (Object) this) instanceof AbstractClientPlayerEntity clientPlayer)) {
 			return;
 		}
 
+		LOGGER.info("normal: {}, {}, {}, fence: {}, {}, {}", playeranimationrework$isIdle, playeranimationrework$isWalking, playeranimationrework$isRunning, playeranimationrework$isFenceIdle,
+				playeranimationrework$isFenceWalking, playeranimationrework$isFenceRunning);
+
+		// Check if the player is idle
 		if (MathHelper.approximatelyEquals(this.getVelocity().horizontalLength(), 0d)) {
-			if (playeranimationrework$isWalking) {
-				PARPlayerEvents.WALK_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isWalking = false;
-			}
-			if (playeranimationrework$isFenceWalking) {
-				PARPlayerEvents.FENCE_WALK_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isFenceWalking = false;
-			}
-
-			if (playeranimationrework$isRunning && !this.isSprinting()) {
-				PARPlayerEvents.RUN_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isRunning = false;
-			}
-			if (playeranimationrework$isFenceRunning && !this.isSprinting()) {
-				PARPlayerEvents.FENCE_RUN_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isFenceRunning = false;
-			}
-
+			// Handle the player going idle
 			if (!playeranimationrework$isIdle) {
-				PARPlayerEvents.IDLE_START.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isIdle = true;
+				PARPlayerEvents.IDLE_START.invoker().onExecute(clientPlayer);
 			}
 			if (!playeranimationrework$isFenceIdle && playeranimationrework$isOnFence()) {
-				PARPlayerEvents.FENCE_IDLE_START.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isFenceIdle = true;
+				PARPlayerEvents.FENCE_IDLE_START.invoker().onExecute(clientPlayer);
+			}
+
+			if (playeranimationrework$isWalking) {
+				PARPlayerEvents.WALK_STOP.invoker().onExecute(clientPlayer);
+			}
+			if (playeranimationrework$isRunning) {
+				PARPlayerEvents.RUN_STOP.invoker().onExecute(clientPlayer);
+			}
+
+			if (playeranimationrework$isFenceWalking) {
+				PARPlayerEvents.FENCE_WALK_STOP.invoker().onExecute(clientPlayer);
+			}
+			if (playeranimationrework$isFenceRunning) {
+				PARPlayerEvents.FENCE_RUN_STOP.invoker().onExecute(clientPlayer);
+			}
+		} else {
+			// Handle the player unidling
+			if (playeranimationrework$isIdle) {
+				PARPlayerEvents.IDLE_STOP.invoker().onExecute(clientPlayer);
+			}
+			if (playeranimationrework$isFenceIdle) {
+				PARPlayerEvents.FENCE_IDLE_STOP.invoker().onExecute(clientPlayer);
 			}
 		}
 	}
 
 	@Inject(method = "travel", at = @At(value = "TAIL"))
 	private void playeranimationrework$invokePlayerStartMovementEvents(Vec3d movementInput, CallbackInfo ci) {
-		if (!(((PlayerEntity) (Object) this) instanceof AbstractClientPlayerEntity)) {
+		if (!(((PlayerEntity) (Object) this) instanceof AbstractClientPlayerEntity clientPlayer) || MathHelper.approximatelyEquals(this.getVelocity().horizontalLength(), 0d)) {
 			return;
 		}
 
-		if (!MathHelper.approximatelyEquals(this.getVelocity().horizontalLength(), 0d)) {
-			if (this.isSprinting()) {
-				if (!playeranimationrework$isRunning) {
-					PARPlayerEvents.RUN_START.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isRunning = true;
-				}
-				if (!playeranimationrework$isFenceRunning && playeranimationrework$isOnFence()) {
-					PARPlayerEvents.FENCE_RUN_START.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isFenceRunning = true;
-				}
-
-				if (playeranimationrework$isWalking) {
-					PARPlayerEvents.WALK_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isWalking = false;
-				}
-				if (playeranimationrework$isFenceWalking) {
-					PARPlayerEvents.FENCE_WALK_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isFenceWalking = false;
-				}
-			} else {
-				if (!playeranimationrework$isWalking) {
-					PARPlayerEvents.WALK_START.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isWalking = true;
-				}
-				if (!playeranimationrework$isFenceWalking && playeranimationrework$isOnFence()) {
-					PARPlayerEvents.FENCE_WALK_START.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isFenceWalking = true;
-				}
-
-				if (playeranimationrework$isRunning) {
-					PARPlayerEvents.RUN_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isRunning = false;
-				}
-				if (playeranimationrework$isFenceRunning) {
-					PARPlayerEvents.FENCE_RUN_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-					playeranimationrework$isFenceRunning = false;
-				}
+		if (this.isSprinting()) {
+			// Handle the player starting sprinting
+			if (!playeranimationrework$isRunning) {
+				PARPlayerEvents.RUN_START.invoker().onExecute(clientPlayer);
 			}
 
-			if (playeranimationrework$isIdle) {
-				PARPlayerEvents.IDLE_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isIdle = false;
+			if (!playeranimationrework$isFenceRunning && playeranimationrework$isOnFence()) {
+				PARPlayerEvents.FENCE_RUN_START.invoker().onExecute(clientPlayer);
 			}
-			if (playeranimationrework$isFenceIdle) {
-				PARPlayerEvents.FENCE_IDLE_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isFenceIdle = false;
+
+			if (playeranimationrework$isWalking) {
+				PARPlayerEvents.WALK_STOP.invoker().onExecute(clientPlayer);
+			}
+
+			if (playeranimationrework$isFenceWalking) {
+				PARPlayerEvents.FENCE_WALK_STOP.invoker().onExecute(clientPlayer);
+			}
+		} else {
+			// Handle the player stopping sprinting without fully stopping moving
+			if (!playeranimationrework$isWalking) {
+				PARPlayerEvents.WALK_START.invoker().onExecute(clientPlayer);
+			}
+
+			if (!playeranimationrework$isFenceWalking && playeranimationrework$isOnFence()) {
+				PARPlayerEvents.FENCE_WALK_START.invoker().onExecute(clientPlayer);
+			}
+
+			if (playeranimationrework$isRunning) {
+				PARPlayerEvents.RUN_STOP.invoker().onExecute(clientPlayer);
+			}
+
+			if (playeranimationrework$isFenceRunning && playeranimationrework$isOnFence()) {
+				PARPlayerEvents.FENCE_RUN_STOP.invoker().onExecute(clientPlayer);
 			}
 		}
 
+		// Check if the player has fallen off a fence
 		if (!playeranimationrework$isOnFence()) {
+			// Handle the player falling off a fence
 			if (playeranimationrework$isFenceWalking) {
-				PARPlayerEvents.FENCE_WALK_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isFenceWalking = false;
+				PARPlayerEvents.FENCE_WALK_STOP.invoker().onExecute(clientPlayer);
 			}
 			if (playeranimationrework$isFenceRunning) {
-				PARPlayerEvents.FENCE_RUN_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isFenceRunning = false;
+				PARPlayerEvents.FENCE_RUN_STOP.invoker().onExecute(clientPlayer);
 			}
 			if (playeranimationrework$isFenceIdle) {
-				PARPlayerEvents.FENCE_IDLE_STOP.invoker().onExecute((AbstractClientPlayerEntity) (Object) this);
-				playeranimationrework$isFenceIdle = false;
+				PARPlayerEvents.FENCE_IDLE_STOP.invoker().onExecute(clientPlayer);
 			}
 		}
 	}
